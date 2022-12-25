75d792f74dfd68328469e0162612176eafb63261
essesoul <essesoul@foxmail.com>
2022-12-25 22:47:39 +0800

    Update runcheck.sh

--- runcheck.sh
+++ runcheck.sh
@@ -5,7 +5,7 @@ curl https://${1} -k -v -s -l 2> ./tmp/ca.info
 cat ./tmp/ca.info | grep 'start date: ' > ./tmp/${1}.info
 cat ./tmp/ca.info | grep 'expire date: ' >> ./tmp/${1}.info
 cat ./tmp/ca.info | grep 'issuer: ' >> ./tmp/${1}.info
-cat ./tmp/ca.info | grep 'SSL certificate verify' >> ./tmp/${1}.info
+#cat ./tmp/ca.info | grep 'SSL certificate verify' >> ./tmp/${1}.info
 cat ./tmp/ca.info | grep 'subject: ' >> ./tmp/${1}.info
 
 sed -i 's|\*  start date: ||g' ./tmp/${1}.info
@@ -17,7 +17,7 @@ sed -i 's|\*  subject: ||g' ./tmp/${1}.info
 start=$(sed -n '1p' ./tmp/${1}.info)
 expire=$(sed -n '2p' ./tmp/${1}.info)
 issuer=$(sed -n '3p' ./tmp/${1}.info)
-status=$(sed -n '4p' ./tmp/${1}.info)
+#status=$(sed -n '4p' ./tmp/${1}.info)
 subject=$(sed -n '5p' ./tmp/${1}.info)
 
 rm -f ./tmp/ca.info
@@ -36,19 +36,19 @@ echo '"start": "'$start'",' >> ./tmp/${1}.json
 echo '"expire": "'$expire'",' >> ./tmp/${1}.json
 echo '"issuer": "'$issuer'",' >> ./tmp/${1}.json
 
-if [ $expirestamp \< $nowstamp ]; then
-    echo '"status": "Expired",' >> ./tmp/${1}.json
-    echo '"statuscolor": "error",' >> ./tmp/${1}.json
-elif [ $expireday \< 10 ]; then
-    echo '"status": "Soon Expired",' >> ./tmp/${1}.json
-    echo '"statuscolor": "warning",' >> ./tmp/${1}.json
-elif [ $status = "ok." ]; then
+#if [ $expirestamp \< $nowstamp ]; then
+#    echo '"status": "Expired",' >> ./tmp/${1}.json
+#    echo '"statuscolor": "error",' >> ./tmp/${1}.json
+# elif [ $expireday \< 10 ]; then
+#     echo '"status": "Soon Expired",' >> ./tmp/${1}.json
+#     echo '"statuscolor": "warning",' >> ./tmp/${1}.json
+# elif [ $status = "ok." ]; then
     echo '"status": "Valid",' >> ./tmp/${1}.json
     echo '"statuscolor": "success",' >> ./tmp/${1}.json
-else
-    echo '"status": "Invalid",' >> ./tmp/${1}.json
-    echo '"statuscolor": "error",' >> ./tmp/${1}.json
-fi
+# else
+#     echo '"status": "Invalid",' >> ./tmp/${1}.json
+#     echo '"statuscolor": "error",' >> ./tmp/${1}.json
+#fi
 
 echo '"check": "'$DATE'",' >> ./tmp/${1}.json
 echo '"remain": "'$expireday'"' >> ./tmp/${1}.json
